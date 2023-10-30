d08e52a263f0982a83115bbc894092bd092a11e5
BetterBite <gabrielgaladyk@yahoo.pl>
2023-10-30 23:00:24 +0000

    put sdl into a struct

--- main.c
+++ main.c
@@ -10,6 +10,12 @@ const int SCREEN_HEIGHT = 480;
 
 typedef enum Phase {LATE_NIGHT, DAWN, MORNING, AFTERNOON, DUSK, EVENING} Phase;
 
+typedef struct display {
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_Surface* surface;
+} display;
+
 int main() {
     time_t globalTime = time(NULL); // Returns as # of seconds since Jan 1st 1970
     if(globalTime != (time_t)(-1)) { // Check if time was properly returned
@@ -26,47 +32,44 @@ int main() {
 
     // Get a renderer! Important! (Probably)
 
-    SDL_Window* window = NULL;
-
+    display screen;
     if(SDL_Init(SDL_INIT_VIDEO) < 0) printf("WE goofed %s\n", SDL_GetError());
     else {
-        window = SDL_CreateWindow("SDL Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-        if (window == NULL) printf("WE done goofed as well %s\n", SDL_GetError());
+        screen.window = SDL_CreateWindow("SDL Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+        if (screen.window == NULL) printf("WE done goofed as well %s\n", SDL_GetError());
     }
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_Surface* surface = NULL;
+    screen.renderer = SDL_CreateRenderer(screen.window, -1, SDL_RENDERER_ACCELERATED);
     switch (phase) {
     case LATE_NIGHT:
-        SDL_SetRenderDrawColor(renderer, 0, 12, 24, 255);
+        SDL_SetRenderDrawColor(screen.renderer, 0, 12, 24, 255);
         break;
     case DAWN:
-        SDL_SetRenderDrawColor(renderer, 255, 165, 0, 255);
+        SDL_SetRenderDrawColor(screen.renderer, 255, 165, 0, 255);
         break;
     case MORNING:
-        SDL_SetRenderDrawColor(renderer, 0, 128, 0, 255);
+        SDL_SetRenderDrawColor(screen.renderer, 0, 128, 0, 255);
         break;
     case AFTERNOON:
-        SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+        SDL_SetRenderDrawColor(screen.renderer, 255, 255, 0, 255);
         break;
     case DUSK:
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_SetRenderDrawColor(screen.renderer, 255, 0, 0, 255);
         break;
     case EVENING:
-        SDL_SetRenderDrawColor(renderer, 128, 0, 128, 255);
+        SDL_SetRenderDrawColor(screen.renderer, 128, 0, 128, 255);
         break;
     default:
         break;
     }
 
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
+    SDL_RenderClear(screen.renderer);
+    SDL_RenderPresent(screen.renderer);
 
-    printf("somethin\n");
     SDL_Delay(4000);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_FreeSurface(surface);
+    SDL_DestroyRenderer(screen.renderer);
+    SDL_DestroyWindow(screen.window);
+    SDL_FreeSurface(screen.surface);
     SDL_Quit();
 
     return 0;
